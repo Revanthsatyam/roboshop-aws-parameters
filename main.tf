510034a1c1ed95960ad6c47b898ebc4a013ff1a7
Revanthsatyam <satyamrevanth@gmail.com>
2024-02-14 11:30:06 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "aws_ssm_parameter" "main" {
   name     = each.key
   type     = each.value["type"]
   value    = each.value["value"]
+  key_id   = "arn:aws:kms:us-east-1:369896808580:key/690f4b64-eb25-45af-8c96-51a2c0efe9d9"
 }
