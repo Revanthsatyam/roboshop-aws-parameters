c5df64c6e22dc5fc4c7cab004b029c39ad485594
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-28 06:17:08 +0530

    Terraform

--- main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ssm_parameter" "main" {
-  for_each = var.parameters
-  name     = each.key
-  type     = each.value["type"]
-  value    = each.value["value"]
-  key_id   = "arn:aws:kms:us-east-1:369896808580:key/690f4b64-eb25-45af-8c96-51a2c0efe9d9"
-}
--- state.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tf-state-dbr74"
-    key    = "aws-parameters/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
