483be08d24d557a1648c8a1bb908690552596b2c
Revanthsatyam <satyamrevanth@gmail.com>
2024-06-06 12:18:21 +0530

    KMS Parameters

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ resource "aws_ssm_parameter" "main" {
   name     = each.key
   type     = each.value["type"]
   value    = each.value["value"]
+  key_id   = "arn:aws:kms:us-east-1:590183653013:key/86c6c9b1-d438-4118-a6c8-1ccc8fc4ce55"
 }
 
